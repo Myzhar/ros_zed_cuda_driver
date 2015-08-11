080dd6c4ded08f6009039c057b5fc4f97fbd811b
Myzhar <myzhar@robot-home.it>
2015-08-11 21:00:44 +0200

    Files to start the project

---
+++ src/zed_camera_node.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <ros/ros.h>
+#include <zed_camera_driver.h>
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "zed_camera_node");
+
+    ROS_INFO_STREAM("-----------------------------------\r");
+    ROS_INFO_STREAM("       StereoLabs ZED node         \r");
+    ROS_INFO_STREAM("-----------------------------------\r");
+
+    //ZedDriver cameraDriver;
+
+    //cameraDriver.run();
+
+    ros::spin();
+
+    ROS_INFO_STREAM( "... shutting down complete." );
+
+    return(EXIT_SUCCESS);
+}
